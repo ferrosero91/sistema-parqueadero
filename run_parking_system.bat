c8354df8c9dc863d93a8f3179179af2ab3683a41
ferrosero91@gmail.com <elier.rosero@campusucc.edu.co>
2025-07-04 16:42:21 -0500

    configuracion

--- run_parking_system.bat
+++ run_parking_system.bat
@@ -1,21 +1,6 @@
 @echo off
-cd /d %~dp0
-
-:: Crear entorno virtual si no existe
-if not exist "venv\" (
-    echo Creando entorno virtual...
-    python -m venv venv
-)
-
-:: Activar entorno virtual
-call venv\Scripts\activate
-
-:: Instalar dependencias
-echo Instalando dependencias...
-pip install --upgrade pip
-pip install -r requirements.txt
 
 :: Iniciar servidor
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver localhost:8000
 start "" http://localhost:8000
 pause
