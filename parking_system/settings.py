9f1d2855a75216352d09369b73a9501a90d75eac
ferrosero91@gmail.com <elier.rosero@campusucc.edu.co>
2025-07-07 19:57:05 -0500

    nbd

--- parking_system/settings.py
+++ parking_system/settings.py
@@ -81,10 +81,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'parking_system.wsgi.application'
 
 # Base de datos con URL fija (la que aparece en Coolify)
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='postgres://postgres:Frt2j39b3m123456@x04cws0g0w4w88gkck4kcgw4:5432/postgres?sslmode=disable'
+#     )
+# }
+
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://postgres:Frt2j39b3m123456@x04cws0g0w4w88gkck4kcgw4:5432/postgres?sslmode=disable'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Frt2j39b3m123456',
+        'HOST': 'x04cws0g0w4w88gkck4kcgw4',
+        'PORT': '5432',
+    }
 }
 
 # Validadores de contraseñas
