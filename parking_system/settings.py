279de71d415018848d5c9dcb4c914afc70ddafcf
ferrosero91 <ferrosero91@gmail.com>
2025-07-14 18:15:48 -0500

    actualizacion settings

--- parking_system/settings.py
+++ parking_system/settings.py
@@ -101,19 +101,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'parking_system.wsgi.application'
 
 # Base de datos
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default="postgres://postgres:s2CJ8wi4dQwA4jP38wrMOKfHPjCyz5fRkGKX7FQ9WaiTLPCsnVxCs0ipe3tAylHq@jc800wgok88ggw4kkcgckgg8:5432/postgres"
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.config(
+        default="postgres://postgres:s2CJ8wi4dQwA4jP38wrMOKfHPjCyz5fRkGKX7FQ9WaiTLPCsnVxCs0ipe3tAylHq@jc800wgok88ggw4kkcgckgg8:5432/postgres"
+    )
+}
 
 # Configuración de base de datos SQLite para desarrollo
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 # Validadores de contraseñas
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
